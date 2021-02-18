04ef4ed8df8cfe719c0e94d301a2f5d500a204d6
Silas Castro <silascastro15@gmail.com>
2021-02-17 22:00:13 -0400

    iniciando server

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ const app = express();
 app.use(express.json());
 app.get('/', (req,res) => {
     res.send({msg: 'hello world'});
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
 })
+
+module.exports = app;
