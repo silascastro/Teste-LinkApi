ce38874747f8fd99be2a105ac9dcd1dfc258ce0c
Silas Castro <silascastro15@gmail.com>
2021-02-21 22:35:33 -0400

    refatorando models

--- src/models/deal.js
+++ src/models/deal.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-const Deal = new Schema({
-    
+var DealSchema = new Schema({
+    id: Number,
+    creator_user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    org_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: String,
+    status: String,
+    date: Date,
+    qtd: Number,
+
+
 });
+
+module.exports = mongoose.model('Deal', DealSchema);
--- src/models/organization.js
+++ src/models/organization.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-const Organization = new Schema({
-    
+const OrganizationSchema = new Schema({
+    name: String,
+    owner_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    cc_email: String,
 });
+
+module.exports = mongoose.model('Organization', OrganizationSchema);
